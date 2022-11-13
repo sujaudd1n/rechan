894fe037a0dcea7e1158c6542503eed8d836b258
Sujauddin <61101752+sujaudd1n@users.noreply.github.com>
2022-11-13 18:36:12 +0530

    Use 4chan api
    4chan api has been used instead of regular expression.
    media query for 1080px
    new color
    Redesign localstorage

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,64 +4,64 @@ if (!localStorage.getItem("urls"))
 
 // Get media from localstorage
 let urls = JSON.parse(localStorage.getItem("urls"));
-let media = [...new Set(urls)];
-let ix = 0;
+let media;
+let ix;
+let total;
 
 let image = document.querySelector(".figure__img");
 let video = document.querySelector(".figure__vid");
 
-// Fetch media 
+// Fetch media
 document.querySelector("form").addEventListener("submit", async (e) => {
   e.preventDefault();
   // create source url
-  proxy_url = "https://cors-anywhere.herokuapp.com/";
-  const url = proxy_url + document.querySelector(".form__url").value;
+  proxy_url = "https://api.allorigins.win/raw?url=";
 
-  let response = await fetch(url);
-  if (response.ok) {
-    let source = await response.text();
-    media = find_urls(source);
-    ix = 0;
-  }
-  else
-  {
+  let b_url = document.querySelector(".form__url").value;
+
+  if (b_url !== "") {
+    b_url_array = b_url.split("/");
+    let board = b_url_array[3];
+    let b_id = b_url_array[5];
+    let url = proxy_url + `https://a.4cdn.org/${board}/thread/${b_id}.json`;
+
+    let response = await fetch(url);
+    if (response.ok) {
+      console.log(response);
+      let source = await response.json();
+      total = 0;
+      media = get_urls(source, board);
+      ix = 0;
+    }
+  } else {
     urls = JSON.parse(localStorage.getItem("urls"));
     media = [...new Set(urls)];
     ix = 0;
+    total = media.length;
   }
-    
-  if (media.length > 0)
-    display();
+
+  if (media.length > 0) display();
 });
 
-function display()
-{
-  let type = get_media_type(media[ix]);
-  if (type === "img")
-    display_image(media[ix]);
-  else
-    display_video(media[ix]);
+function display() {
+  let type = get_media_type(media[ix].url);
+  if (type === "img") display_image(media[ix]);
+  else display_video(media[ix]);
+  document.querySelector(".figure__total").textContent =
+    (ix + 1).toString() + " / " + total.toString();
 }
 
-function get_media_type(url)
-{
-  if (
-    url.endsWith("jpg") ||
-    url.endsWith("gif") ||
-    url.endsWith("jpeg")
-  ) 
+function get_media_type(url) {
+  if (url.endsWith("jpg") || url.endsWith("gif") || url.endsWith("jpeg"))
     return "img";
-    return "video";
+  return "video";
 }
-
 
 function f_download() {
   let link = document.createElement("a");
   let type = get_media_type(media[ix]);
-  if (type === "img")
-    link.href = image.src;
-  else
-  {
+  if (type === "img") link.href = image.src;
+  else {
     link.href = video.src;
     video.pause();
   }
@@ -72,16 +72,12 @@ function f_download() {
 }
 
 function f_save() {
-  let type = get_media_type(media[ix]);
-  if (type === "img")
-    url = document.querySelector(".figure__img").src;
-  else
-    url = document.querySelector(".figure__vid").src;
-    urls.push(url);
-    localStorage.setItem("urls", JSON.stringify(urls));
-    console.log(urls);
+  let type = get_media_type(media[ix].url);
+  if (type === "img") url = document.querySelector(".figure__img").src;
+  else url = document.querySelector(".figure__vid").src;
+  urls.push({url: url, filename: media[ix].filename});
+  localStorage.setItem("urls", JSON.stringify(urls));
 }
-
 
 function f_play() {
   document.querySelector(".figure__vid").play();
@@ -102,16 +98,16 @@ function f_prev() {
 }
 
 function process_pointerdown(e) {
-  if (e.layerX > e.target.offsetWidth / 2)
-    ix = ++ix % media.length;
-  else
-    --ix < 0 ? (ix = media.length - 1) : 1;
+  if (e.layerX > e.target.offsetWidth / 2) ix = ++ix % media.length;
+  else --ix < 0 ? (ix = media.length - 1) : 1;
 
   display();
 }
 
 function display_image(url) {
-  document.querySelector(".figure__img").src = url;
+  document.querySelector(".figure__img").src = url.url;
+  document.querySelector(".figure__name").textContent =
+    "Filename: " + url.filename;
   document.querySelector(".figure__img").style.display = "block";
   document.querySelector(".figure__vid").style.display = "none";
   document.querySelector(".figure__vid").pause();
@@ -119,7 +115,9 @@ function display_image(url) {
   document.querySelector(".figure__pause").style.display = "none";
 }
 function display_video(url) {
-  document.querySelector(".figure__vid").src = url;
+  document.querySelector(".figure__vid").src = url.url;
+  document.querySelector(".figure__name").textContent =
+    "Filename: " + url.filename;
   document.querySelector(".figure__vid").style.display = "block";
   document.querySelector(".figure__play").style.display = "block";
   document.querySelector(".figure__pause").style.display = "block";
@@ -127,12 +125,15 @@ function display_video(url) {
   document.querySelector(".figure__vid").play();
   document.querySelector(".figure__vid").loop = true;
 }
-function find_urls(source) {
-  let matches = source.matchAll(/(\/\/i.4cdn([/|.|\w|\s|-])*(.jpg|.webm|.jpeg|.gif))/g);
+function get_urls(data, board) {
   let media = [];
-  for (let td of matches) {
-    url = "https:" + td[0];
-    if (!url.endsWith("s.jpg")) media.push(url);
+  for (let td of data["posts"]) {
+    if ("ext" in td) {
+      m_url = `https://i.4cdn.org/${board}/${td.tim}${td.ext}`;
+      m_filename = td.filename;
+      media.push({ url: m_url, filename: m_filename });
+      total++;
+    }
   }
   return [...new Set(media)];
 }
@@ -142,7 +143,9 @@ document.querySelector(".figure__next").addEventListener("click", f_next);
 document.querySelector(".figure__prev").addEventListener("click", f_prev);
 document.querySelector(".figure__play").addEventListener("click", f_play);
 document.querySelector(".figure__pause").addEventListener("click", f_pause);
-document.querySelector(".figure__download").addEventListener("click", f_download);
+document
+  .querySelector(".figure__download")
+  .addEventListener("click", f_download);
 document.querySelector(".figure__save").addEventListener("click", f_save);
 image.addEventListener("pointerdown", process_pointerdown);
 video.addEventListener("pointerdown", process_pointerdown);
